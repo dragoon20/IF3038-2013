e34656f17b2f2bf450b2d6a2069641d3d81a3967
dragoon20 <fernandojordan.92@gmail.com>
2013-04-25 11:02:36 +0700

    A lot of changes for Task 4

--- WebContent/js/edit_profile.js
+++
@@ -1,9 +0,0 @@
-Rp(function() {
-	var original = Rp('#edit_form').serialize();
-	Rp('#edit_form').on('submit', function() {
-		var now = Rp('#edit_form').serialize();
-		if (original == now)
-			alert('Tidak ada perubahan.');
-		this.submit();
-	})
-})
